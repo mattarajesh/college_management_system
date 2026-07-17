51c871693a46524eb3fa1e2af5b6472e741991c6
mattarajesh <mattarajesh8391@gmail.com>
2026-07-17 14:15:47 +0530

    create add student page

--- student_management/students/views.py
+++ student_management/students/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import student
 
 # Add this exact function block
 def home(request):
     return render(request, 'students/home.html')
+def add_student(request):
+    if request.method == "POST":
+        name = request.POST["name"]
+        age = request.POST["age"]
+        email = request.POST["email"]
+
+        student.objects.create(
+            name = name,
+            age = age,
+            email = email
+        )
+        return redirect("home")
+    return render(request,"students/add_student.html")
